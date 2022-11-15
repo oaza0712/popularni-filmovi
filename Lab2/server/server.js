c3f700c5fda8e2fc0ab5522ff38448c0280d251e
Leona07 <leona.varazdin@gmail.com>
2022-11-15 02:56:05 +0100

    lab2

---
+++ Lab2/server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const { Client } = require('pg');
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "bazepodataka",
+    database: "Popularni_filmovi"
+});
+
+client.connect();
+
+client.query(" (SELECT array_to_json(array_agg(row_to_json(t))) FROM (SELECT film.ime_film, film.datum_izdavanja, film.ocjena, film.trajanje_min,(select array_to_json(array_agg(row_to_json(d))) from (select ime_zarn from je_zarn natural join zarn where id_film=film.id_film) d) as zarn,(select array_to_json(array_agg(row_to_json(d))) from (select ime_drzava from sniman_u natural join drzava where id_film=film.id_film) d) as drzava,(select array_to_json(array_agg(row_to_json(d))) from (select ime_redatelj, prezime_redatelj from redatelji natural join je_redatelj where id_film=film.id_film) d) as redatelj,(select array_to_json(array_agg(row_to_json(d))) from (select ime_glumac, prezime_glumac from glumi_u natural join glumci where id_film=film.id_film) d) as glumci,(select array_to_json(array_agg(row_to_json(d))) from (select ime_nagrada, datum_osvajanja from je_osvojio natural join nagrada where id_film=film.id_film) d) as nagrade,film.box_office,film.budzet FROM film) t )", (err, { rows })=>{
+
+    if ( err ){
+        console.log({ err });
+    } else {
+        
+        console.log(rows[0]);
+    }
+    
+    app.get('/json_table', (req,res) => {
+        res.json(rows);
+    });
+    client.end();
+    
+});
+
+app.get('/message', (req, res) => {
+    res.json({ message: "Hello from server2!" });
+});
+
+app.get('/download_json', (req, res) => res.download('./popularni-filmovi.json'));
+app.get('/download_csv', (req, res) => res.download('./popularni-filmovi.csv'));
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+  });
