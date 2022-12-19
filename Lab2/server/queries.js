5c99b5ce6c19aca06b9a06b55a6afd0ddb363353
Leona07 <leona.varazdin@gmail.com>
2022-12-19 21:13:53 +0100

    lab3

---
+++ Lab2/server/queries.js
@@ -0,0 +1,223 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "bazepodataka",
+    database: "Popularni_filmovi"
+});
+
+const getAllMovies = (request, response) => {
+  pool.query(
+    " (SELECT array_to_json(array_agg(row_to_json(t))) FROM (SELECT film.ime_film, film.datum_izdavanja, film.ocjena, film.trajanje_min,(select array_to_json(array_agg(row_to_json(d))) from (select ime_zarn from je_zarn natural join zarn where id_film=film.id_film) d) as zarn,(select array_to_json(array_agg(row_to_json(d))) from (select ime_drzava from sniman_u natural join drzava where id_film=film.id_film) d) as drzava,(select array_to_json(array_agg(row_to_json(d))) from (select ime_redatelj, prezime_redatelj from redatelji natural join je_redatelj where id_film=film.id_film) d) as redatelj,(select array_to_json(array_agg(row_to_json(d))) from (select ime_glumac, prezime_glumac from glumi_u natural join glumci where id_film=film.id_film) d) as glumci,(select array_to_json(array_agg(row_to_json(d))) from (select ime_nagrada, datum_osvajanja from je_osvojio natural join nagrada where id_film=film.id_film) d) as nagrade,film.box_office,film.budzet FROM film) t ) ",
+
+    (error, results) => {
+      if (error) {
+       
+        throw error;
+      }
+      response.status(200).send(`{
+        "status": "OK",
+        "message": "Fetched all movies",
+        "reponse": {
+          ${JSON.stringify(results.rows)}
+        }
+       }`);
+    }
+  );
+};
+const getMovieById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+  pool.query(
+    " (SELECT array_to_json(array_agg(row_to_json(t))) FROM (SELECT film.ime_film, film.datum_izdavanja, film.ocjena, film.trajanje_min,(select array_to_json(array_agg(row_to_json(d))) from (select ime_zarn from je_zarn natural join zarn where id_film=film.id_film) d) as zarn,(select array_to_json(array_agg(row_to_json(d))) from (select ime_drzava from sniman_u natural join drzava where id_film=film.id_film) d) as drzava,(select array_to_json(array_agg(row_to_json(d))) from (select ime_redatelj, prezime_redatelj from redatelji natural join je_redatelj where id_film=film.id_film) d) as redatelj,(select array_to_json(array_agg(row_to_json(d))) from (select ime_glumac, prezime_glumac from glumi_u natural join glumci where id_film=film.id_film) d) as glumci,(select array_to_json(array_agg(row_to_json(d))) from (select ime_nagrada, datum_osvajanja from je_osvojio natural join nagrada where id_film=film.id_film) d) as nagrade,film.box_office,film.budzet FROM film WHERE id_film = $1) t ) ",
+    [id],
+    (error, results) => {
+      if (error) {
+        
+        throw error;
+      }
+      let proba = JSON.stringify(results.rows);
+      let status = 'OK';
+      let message = 'Fetched movie by id,' ;
+      let returnStatus = 200;
+      console.log('REsults rows ', results.rows[0]["array_to_json"]);
+      
+        if(results.rows[0]["array_to_json"]==null){
+          returnStatus = 404;
+          status = '404 not found,';
+          message = 'Movie with that id doesnt exist,'
+          proba = 'null'
+        }
+        
+      response.status(returnStatus).send((`{
+        "status": ${status}
+        "message": ${message}
+        "reponse": {
+          ${proba}
+        }
+       }`));
+    }
+  );
+};
+
+const getActors = (request, response) => {
+  pool.query(
+    " (SELECT * from glumci)",
+
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`{
+        "status": "OK",
+        "message": "Fetched all actors",
+        "reponse": {
+          ${JSON.stringify(results.rows)}
+        }
+       }`);
+    }
+  );}
+
+  const getDirectors = (request, response) => {
+    pool.query(
+      " (SELECT * from redatelji)",
+  
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(200).send(`{
+          "status": "OK",
+          "message": "Fetched all directors",
+          "reponse": {
+            ${JSON.stringify(results.rows)}
+          }
+         }`);
+      }
+    );}
+
+
+    const getAwards = (request, response) => {
+      pool.query(
+        " (SELECT * from nagrada)",
+    
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          response.status(200).send(`{
+            "status": "OK",
+            "message": "Fetched all awards",
+            "reponse": {
+              ${JSON.stringify(results.rows)}
+            }
+           }`);
+        }
+      );}
+
+
+const createMovie = (request, response) => {
+  var {ime_drzava} = request.body
+  
+  pool.query('INSERT INTO public.drzava (ime_drzava) VALUES ($1) RETURNING id_drzava;' + '', [ ime_drzava ], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`{
+      "status": "OK",
+      "message": "Created country object",
+      "reponse": {
+      "id_drzava":  ${results.rows[0].id_drzava}
+      }
+     }`)
+  })
+}
+
+const updateMovie = (request, response) => {
+  const id_drzava = parseInt(request.params.id)
+  var {ime_drzava} = request.body
+  let proba = id_drzava;
+  let status = 'OK';
+  let message = 'Updated country by id,' ;
+  let returnStatus = 200;
+  let flag = false;
+    pool.query("Select id_drzava from drzava ",  (error, results) => {
+        
+       for(let i = 0; i < results.rowCount; i++){
+        if (id_drzava == results.rows[i].id_drzava ){
+          flag = true;
+        }
+       }
+       if(!flag){
+        console.log('nema tog id');
+        returnStatus = 404;
+          status = '404 not found,';
+          message = 'Country with that id doesnt exist,'
+          proba = 'null'
+       }
+        
+      })
+ 
+
+  pool.query(
+    'UPDATE public.drzava SET ime_drzava = $1 WHERE id_drzava = $2 RETURNING id_drzava',
+    [ime_drzava,  id_drzava],
+    (error, results) => {
+        
+      response.status(returnStatus).send((`{
+        "status": ${status}
+        "message": ${message}
+        "reponse": {
+          ${proba}
+        }
+       }`));
+    }
+  )
+}
+
+const deleteMovie = (request, response) => {
+  const id_drzava = parseInt(request.params.id)
+  let proba = id_drzava;
+  let status = 'OK';
+  let message = 'Deleted country by id,' ;
+  let returnStatus = 200;
+  let flag = false;
+    pool.query("Select id_drzava from drzava ",  (error, results) => {
+        
+       for(let i = 0; i < results.rowCount; i++){
+        if (id_drzava == results.rows[i].id_drzava ){
+          flag = true;
+        }
+       }
+       if(!flag){
+        console.log('nema tog id');
+        returnStatus = 404;
+          status = '404 not found,';
+          message = 'Country with that id doesnt exist,'
+          proba = 'null'
+       }
+        
+      })
+  pool.query('DELETE FROM drzava WHERE id_drzava = $1', [id_drzava], (error, results) => {
+      
+    response.status(returnStatus).send((`{
+      "status": ${status}
+      "message": ${message}
+      "reponse": {
+        ${proba}
+      }
+     }`));
+  })
+}
+
+module.exports = {
+getAllMovies,
+getMovieById,
+createMovie,
+updateMovie,
+deleteMovie,
+getActors,
+getDirectors,
+getAwards
+};
--- Lab2/server/server.js
+++ Lab2/server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const db = require('./queries')
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 const { Client } = require('pg');
 
@@ -17,7 +25,6 @@ const client = new Client({
 });
 
 client.connect();
-
 client.query(" (SELECT array_to_json(array_agg(row_to_json(t))) FROM (SELECT film.ime_film, film.datum_izdavanja, film.ocjena, film.trajanje_min,(select array_to_json(array_agg(row_to_json(d))) from (select ime_zarn from je_zarn natural join zarn where id_film=film.id_film) d) as zarn,(select array_to_json(array_agg(row_to_json(d))) from (select ime_drzava from sniman_u natural join drzava where id_film=film.id_film) d) as drzava,(select array_to_json(array_agg(row_to_json(d))) from (select ime_redatelj, prezime_redatelj from redatelji natural join je_redatelj where id_film=film.id_film) d) as redatelj,(select array_to_json(array_agg(row_to_json(d))) from (select ime_glumac, prezime_glumac from glumi_u natural join glumci where id_film=film.id_film) d) as glumci,(select array_to_json(array_agg(row_to_json(d))) from (select ime_nagrada, datum_osvajanja from je_osvojio natural join nagrada where id_film=film.id_film) d) as nagrade,film.box_office,film.budzet FROM film) t )", (err, { rows })=>{
 
     if ( err ){
@@ -26,21 +33,35 @@ client.query(" (SELECT array_to_json(array_agg(row_to_json(t))) FROM (SELECT fil
         
         console.log(rows[0]);
     }
+
+
     
     app.get('/json_table', (req,res) => {
-        res.json(rows);
+         res.json(rows);
     });
+
     client.end();
     
 });
 
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello from server2!" });
-});
 
 app.get('/download_json', (req, res) => res.download('./popularni-filmovi.json'));
 app.get('/download_csv', (req, res) => res.download('./popularni-filmovi.csv'));
 
+app.get('/movies/all_movies', db.getAllMovies)
+app.get('/movies/:id', db.getMovieById)
+
+app.get('/actors', db.getActors)
+app.get('/directors', db.getDirectors)
+app.get('/awards', db.getAwards)
+
+app.post('/movies', db.createMovie)
+app.put('/movies/:id', db.updateMovie)
+app.delete('/movies/:id', db.deleteMovie)
+
+
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`);
   });
+
+ 
